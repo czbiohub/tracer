1d9d3a5fa1ac349f632b9bcaf2f6bd211af5f4f5
Aaron McGeever <aaron.mcgeever@gmail.com>
2019-02-24 23:26:52 -0800

    WIP

--- docker_helper_files/docker_wrapper.sh
+++ docker_helper_files/docker_wrapper.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
-set -e
 
-#the environmental variable thing apparently can't be set when making the container
+set -e
+set -x
+
 export IGDATA=/ncbi-igblast-1.7.0/bin
-tracer ${@:1}
+
+S3_FASTQ1_PATH=$1
+S3_FASTQ2_PATH=$2
+SAMPLE_ID=$3
+OUTPUT_BUCKET_PATH=$4
+
+aws s3 cp $S3_FASTQ1_PATH .
+aws s3 cp $S3_FASTQ2_PATH .
+
+gunzip -f *.fastq.gz
+
+READ1=$(basename $S3_FASTQ1_PATH | sed 's/.gz//g')
+READ2=$(basename $S3_FASTQ2_PATH | sed 's/.gz//g')
+
+cp /tracer/docker_helper_files/docker_tracer.conf ~/.tracerrc
+
+tracer assemble --ncores 4 --species Mmus $READ1 $READ2 $SAMPLE_ID $SAMPLE_ID
+
+aws s3 sync $SAMPLE_ID $OUTPUT_BUCKET_PATH
